22308ad4a98d24a5dec50c8a2f7f5691b8a7238e
Victor Eronmosele <victoreronmosele@gmail.com>
2021-01-24 10:21:58 +0100

    feat: add message factory

--- lib/src/util/logger.dart
+++ lib/src/util/logger.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter_native_strings/src/models/message_factory_i.dart';
 import 'package:meta/meta.dart';
 
 class Logger implements LoggerI {
@@ -8,11 +9,11 @@ class Logger implements LoggerI {
   Logger({@required this.ioSink});
 
   @override
-  void printMessage({@required String message}) {
-    ioSink.writeln(message);
+  void printMessage({@required MessageFactoryI messageFactory}) {
+    ioSink.writeln(messageFactory.getMessage());
   }
 }
 
 abstract class LoggerI {
-  void printMessage({@required String message});
+  void printMessage({@required MessageFactoryI messageFactory});
 }
