01b268e94ef9da5591c13adfc49fae7c700e5c75
Victor Eronmosele <victoreronmosele@gmail.com>
2021-03-13 11:50:19 +0100

    feat(android-string-resource): read arb file at 'assets/en.arb'

--- bin/main.dart
+++ bin/main.dart
@@ -1,14 +1,18 @@
+import 'dart:convert' as json;
 import 'dart:io';
 
 import 'package:flutter_native_strings/flutter_native_strings.dart';
 
-void main(List<String> args) {
+Future<void> main(List<String> args) async {
   final LoggerI _logger = Logger(ioSink: IOSink(stdout));
 
   _logger.printMessage(message: 'Welcome to Flutter Native Strings v0.0.1');
 
   try {
-    final File file = File('assets/en.arb');
+    final File arbFile = File('assets/en.arb');
+    final String arbFileContentAsString = arbFile.readAsStringSync();
+    final Map<String, dynamic> arbFileContentAsMap =
+        json.jsonDecode(arbFileContentAsString);
   } catch (e) {
     _logger.printMessage(message: 'Error occured: $e');
   }
